75a455bd0553ee2879572fc6451d40ba3bf85f47
Loui Sakaki <e1352207@outlook.jp>
2018-05-10 01:41:08 +0900

    Refactor a bit.

--- src/cards/ordered/OrderedCards.java
+++ src/cards/ordered/OrderedCards.java
@@ -10,10 +10,11 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Stream;
 
 public class OrderedCards extends Cards {
+    //
+    // Generate methods
     public OrderedCards(String name) {
         this(name, Observer.STUB);
     }
@@ -22,51 +23,51 @@ public class OrderedCards extends Cards {
     }
 
     //
+    // Class-specific methods
     public final void shuffle() {
         Collections.shuffle(this.cards_);
     }
 
     //
-    @Override
-    protected final Card pick() {
-        return cards_.remove(0);
-    }
-    @Override
-    protected final Card pick(CardImitator purpose) {
-        Optional<Card> ret = this.stream()
-            .filter(purpose::isEquivalent)
-            .findFirst()
-        ;
-
-        if(ret.isPresent()) {
-            return CollectionUtil.popElem(
-                this.cards_, ret.get()
-            );
-        }
-
-        throw new CardNotFoundException();
-    }
-    @Override
-    protected final void add(Card card) {
-        cards_.add(card);
-    }
-
+    // Check methods
     @Override
     public final int countCard() {
         return cards_.size();
     }
 
     //
-    @Override
-    public final Stream<Card> stream() {
-        return cards_.stream();
-    }
+    // Iterate methods
     @Override
     public final Iterator<Card> iterator() {
         return cards_.iterator();
     }
+    @Override
+    public final Stream<Card> stream() {
+        return cards_.stream();
+    }
 
     //
+    // Methods related drawing
+    @Override
+    protected final Card pick() {
+        return cards_.remove(0);
+    }
+    @Override
+    protected final Card pick(CardImitator purpose) {
+        return CollectionUtil.popElem(
+            cards_,
+            stream()
+                .filter(purpose::isEquivalent)
+                .findFirst()
+                .orElseThrow(CardNotFoundException::new)
+        );
+    }
+    @Override
+    protected final void add(Card card) {
+        cards_.add(card);
+    }
+
     //
+    // Fields
     private final List<Card> cards_ = new ArrayList<>();
 }
