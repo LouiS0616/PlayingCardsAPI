24a4afe4b974cdd0cadf6ea698995767150c6c5f
Loui Sakaki <e1352207@outlook.jp>
2018-05-11 21:19:40 +0900

    Correct comment.

--- src/card/imitator/wild/WildJokerImitator.java
+++ src/card/imitator/wild/WildJokerImitator.java
@@ -3,7 +3,7 @@ package card.imitator.wild;
 import card.Card;
 import card.Joker;
 
-// If Java allows Diamond Inheritance, this class is no longer necessary.
+// If Java allows Multiple Inheritance, this class is no longer necessary.
 public class WildJokerImitator extends WildCardImitator {
     @Override
     public boolean isEquivalent(Card card) {
