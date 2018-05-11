8ef6e16679ab72752bd41df59009684cbcf401ca
Loui Sakaki <e1352207@outlook.jp>
2018-05-12 03:19:23 +0900

    Refactor a bit.

--- src/observer/PrintObserver.java
+++ src/observer/PrintObserver.java
@@ -7,14 +7,14 @@ import cards.Observer;
 public class PrintObserver implements Observer {
     public void update(Type type, Card card, Cards self, Cards other) {
         switch(type) {
-            case ADD:  print(card, self, other); break;
-            case PICK: print(card, other, self); break;
+            case ADD:  print(card, other, self); break;
+            case PICK: print(card, self, other); break;
         }
     }
 
-    private void print(Card card, Cards self, Cards other) {
+    private void print(Card card, Cards from, Cards to) {
         System.out.println(
-            String.format("Pick %s from %s to %s.", card, other, self)
+            String.format("Pick %s from %s to %s.", card, from, to)
         );
     }
 }
